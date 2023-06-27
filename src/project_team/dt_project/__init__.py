b617321d9f0340ebc068b7d06c09caff3d6fd43a
gkuling <gkuling@gmail.com>
2023-06-27 10:15:09 -0400

    Update an init file

--- src/project_team/dt_project/__init__.py
+++ src/project_team/dt_project/__init__.py
@@ -1,5 +1,6 @@
 from .DataProcessors.Image_Processor import *
 from .DataProcessors.Text_Processor import *
+from .DataProcessors.SITK_Processor import *
 
 from .dt_processing.img_shape import *
 from .dt_processing.img_augmentation import *
