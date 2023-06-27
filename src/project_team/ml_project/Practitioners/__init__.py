70da0b73c091f3419229496ca13eccfa4281b086
gkuling <gkuling@gmail.com>
2023-06-27 10:57:49 -0400

    Update an init file

--- src/project_team/ml_project/Practitioners/__init__.py
+++ src/project_team/ml_project/Practitioners/__init__.py
@@ -4,3 +4,4 @@ from .PTClassification_Practitioner import *
 from .PTRegression_Practitioner import *
 from .ROCAnalysis_Practitioner import *
 from .UNet_Practitioner import *
+from .Ordinal_Correlation_Practitioner import *
