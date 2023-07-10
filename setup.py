537067483c1464cd0ca570d31312407499f441c3
gkuling <gkuling@gmail.com>
2023-07-10 15:19:32 -0400

    Finished updates from the three main projects of the thesis. Committing these files now

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='project_team',
-    version='0.0.2',
+    version='0.1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     url='',
