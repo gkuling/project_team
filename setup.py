d00fd4487d8223633ea5a7c828e48c816921e625
gkuling <gkuling@gmail.com>
2024-01-23 11:31:47 -0500

    Forgot to update the version code :(

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='project_team',
-    version='0.1.15',
+    version='0.1.16',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     url='',
